367eb841d6212625bcf42b5425953bcba1aab83f
Mohamed Yusuf <mymohamedyusuf@gmail.com>
2024-10-08 14:13:27 -0500

    added landing page navbar

---
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const LandingPageNavbar = (props: Props) => {
+  return (
+    <div className='w-full flex justify-between sticky top-0 items-center py-5 z-50'>
+      LandingPageNavbar
+    </div>
+  )
+}
+
+export default LandingPageNavbar
