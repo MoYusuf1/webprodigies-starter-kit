bbbe4b4d51bc3f590e19774dd02442bd6b57c398
Mohamed Yusuf <mymohamedyusuf@gmail.com>
2024-10-08 14:05:53 -0500

    changed env

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const LandingPageLayout = ({children}: {children: React.ReactNode}) => {
+  return <div className="flex flex-col container relative">{childen}</div>
+}
+
+export default LandingPageLayout
