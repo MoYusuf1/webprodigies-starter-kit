364d0c98188c43fa9559551c008050ae10379028
Mohamed Yusuf <mymohamedyusuf@gmail.com>
2024-10-08 14:25:56 -0500

    ok

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Menu from './menu'
 
 type Props = {}
 
 const LandingPageNavbar = (props: Props) => {
   return (
     <div className='w-full flex justify-between sticky top-0 items-center py-5 z-50'>
-      LandingPageNavbar
+      <p className='font-bold text-2xl'>Grouple</p>
+      <Menu orientation={"desktop"}/>
     </div>
   )
 }
---
+++ src/app/(landing)/_components/navbar/menu.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from 'react'
+import { useNavigation } from 'react-day-picker'
+
+type MenuProps = {
+  orientation: "mobile" | "desktop"
+}
+
+type Props = { orientation: MenuProps}
+
+const menu = (props: Props) => {
+  const {section, OnSetSection} = useNavigation()
+  
+  return <div>menu</div>
+
+}
+
+export default menu
