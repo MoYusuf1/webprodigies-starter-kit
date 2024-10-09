7dfa47038f0d8f9a7bb804a13857970ea0089e70
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-10-09 16:59:44 -0500

    finished navbar but glassdoor component half renders

--- src/app/(landing)/_components/navbar/index.tsx
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react'
-import Menu from './menu'
+import { Button } from "@/components/ui/button"
+import { Logout } from "@/icons/logout"
+import Link from "next/link"
+import Menu from "./menu"
+import GlassSheet from "@/components/global/glass-sheet"
+import { MenuIcon } from "lucide-react"
 
 type Props = {}
 
 const LandingPageNavbar = (props: Props) => {
-  return (
-    <div className='w-full flex justify-between sticky top-0 items-center py-5 z-50'>
-      <p className='font-bold text-2xl'>Grouple</p>
-      <Menu orientation={"desktop"}/>
-    </div>
-  )
+    return (
+        <div className="w-full flex justify-between sticky top-0 items-center py-5 z-50">
+            <p className="font-bold text-2xl">Grouple</p>
+            <Menu orientation={"desktop"} />
+            <div className="flex gap-2">
+                <Link href="/sign-in">
+                    <Button
+                        variant="outline"
+                        className="bg-themeBlack rounded-2xl flex gap-2 border-themeGrap hover:bg-themeGray"
+                    >
+                        <Logout />
+                        Login
+                    </Button>
+
+                    <GlassSheet
+                        triggerClass="lg:hidden"
+                        trigger={
+                            <Button
+                                variant={"ghost"}
+                                className="hover:bg-transparent"
+                            >
+                                <MenuIcon size={30} />
+                            </Button>
+                        }
+                    >
+                        <Menu orientation="mobile" />
+                    </GlassSheet>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default LandingPageNavbar
