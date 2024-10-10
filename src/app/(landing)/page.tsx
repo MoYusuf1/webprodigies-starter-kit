a9b0d408a30d7eb868944ff735d48894c0a4c578
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-10-10 14:59:58 -0500

    added dashboard snippet image component. It seems we make components for every little thing in the project lol

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image"
 import CallToAction from "./_components/call-to-action"
+import DashboardSnippet from "./_components/dashboard-snipper"
 
 export default function Home() {
     return (
         <main className="md:px-10 py-20 flex-col gap-36">
             <div>
                 <CallToAction/>
+                <DashboardSnippet />
             </div>
         </main>
     )
