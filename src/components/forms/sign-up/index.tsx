30de6a50707eca80d0d2eb1c8bea41757f513001
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-10-13 01:40:32 -0500

    added another verification portion to the sign up component

--- src/components/forms/sign-up/index.tsx
+++ src/components/forms/sign-up/index.tsx
@@ -1,10 +1,10 @@
 "use client"
-
-import { FormGenerator } from '@/components/global/form-generator'
-import { GROUPLE_CONSTANTS } from '@/constants'
-import { useAuthSignUp } from '@/hooks/authentication'
-import dynamic from 'next/dynamic'
-import React from 'react'
+import { FormGenerator } from "@/components/global/form-generator"
+import { Loader } from "@/components/global/loader"
+import { Button } from "@/components/ui/button"
+import { GROUPLE_CONSTANTS } from "@/constants"
+import { useAuthSignUp } from "@/hooks/authentication"
+import dynamic from "next/dynamic"
 
 type Props = {}
 
@@ -13,42 +13,59 @@ const OtpInput = dynamic(
     import("@/components/global/otp-input").then(
       (component) => component.default,
     ),
-    {ssr:false}
+  { ssr: false },
 )
 
-const SignInForm = (props: Props) => {
+const SignUpForm = (props: Props) => {
   const {
     register,
     errors,
     verifying,
-    creating,  
+    creating,
     onGenerateCode,
     onInitiateUserRegistration,
     code,
     setCode,
-    getValues
+    getValues,
   } = useAuthSignUp()
+
   return (
     <form
       onSubmit={onInitiateUserRegistration}
-      className='flex flex-col gap-3 mt-10'
+      className="flex flex-col gap-3 mt-10"
     >
-       {verifying ? (
-          <div className='flex justify-center mb-5'>
-            <OtpInput otp={code} setOtp={setCode} />
-          </div>
-       ): (
-          GROUPLE_CONSTANTS.signUpForm.map((field) => (
-            <FormGenerator 
-              {...field}
-              key={field.id}
-              register={register}
-              errors={errors}
-            />
-          ))
-       )}
+      {verifying ? (
+        <div className="flex justify-center mb-5">
+          <OtpInput otp={code} setOtp={setCode} />
+        </div>
+      ) : (
+        GROUPLE_CONSTANTS.signUpForm.map((field) => (
+          <FormGenerator
+            {...field}
+            key={field.id}
+            register={register}
+            errors={errors}
+          />
+        ))
+      )}
+
+      {verifying ? (
+        <Button type="submit" className="rounded-2xl">
+          <Loader loading={creating}>Sign Up with Email</Loader>
+        </Button>
+      ) : (
+        <Button
+          type="button"
+          className="rounded-2xl"
+          onClick={() =>
+            onGenerateCode(getValues("email"), getValues("password"))
+          }
+        >
+          <Loader loading={false}>Generate Code</Loader>
+        </Button>
+      )}
     </form>
   )
 }
 
-export default SignInForm
+export default SignUpForm
