509db077324705f1d830f085542334c42561a254
Mohamed Yusuf <101303178+MoYusuf1@users.noreply.github.com>
2024-10-15 13:08:35 -0500

    i fixed it by literally copy and pasting this file wth

--- src/components/forms/sign-in/index.tsx
+++ src/components/forms/sign-in/index.tsx
@@ -1,26 +1,31 @@
-// import { GoogleAuthButton } from "@/components/global/google-oauth-button"
-import SignInForm from "@/components/forms/sign-in"
-import { GoogleAuthButton } from "@/components/global/google-oath-button"
-import { Separator } from "@/components/ui/separator"
+"use client"
 
-const SignInPage = () => {
+import { FormGenerator } from "@/components/global/form-generator"
+import { Loader } from "@/components/global/loader"
+import { Button } from "@/components/ui/button"
+import { GROUPLE_CONSTANTS } from "@/constants"
+import { useAuthSignIn } from "@/hooks/authentication"
+
+type Props = {}
+
+const SignInForm = (props: Props) => {
+  const { isPending, onAuthenticateUser, register, errors } = useAuthSignIn()
+
   return (
-    <>
-      <h5 className="font-bold text-base text-themeTextWhite">Login</h5>
-      <p className="text-themeTextGray leading-tight">
-        Network with people from around the world, join groups, create your own,
-        watch courses and become the best version of yourself.
-      </p>
-      <SignInForm />
-      <div className="my-10 w-full relative">
-        <div className="bg-black p-3 absolute text-themeTextGray text-xs top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          OR CONTINUE WITH
-        </div>
-        <Separator orientation="horizontal" className="bg-themeGray" />
-      </div>
-      <GoogleAuthButton method="signin" />
-    </>
+    <form className="flex flex-col gap-3 mt-10" onSubmit={onAuthenticateUser}>
+      {GROUPLE_CONSTANTS.signInForm.map((field) => (
+        <FormGenerator
+          {...field}
+          key={field.id}
+          register={register}
+          errors={errors}
+        />
+      ))}
+      <Button type="submit" className="rounded-2xl">
+        <Loader loading={isPending}>Sign In with Email</Loader>
+      </Button>
+    </form>
   )
 }
 
-export default SignInPage
+export default SignInForm
